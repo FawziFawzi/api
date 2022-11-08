73328353648e13ee5db108a7882019c1916ab9b9
FawziFawzi <fawzi.fawzi2020@gmail.com>
2022-11-08 16:28:10 +0200

    DB also added after migration

---
+++ db/migrations.sql
@@ -0,0 +1,6 @@
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES (1, '2014_10_12_000000_create_users_table', 1);
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES (2, '2014_10_12_100000_create_password_resets_table', 1);
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES (3, '2019_08_19_000000_create_failed_jobs_table', 1);
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES (4, '2019_12_14_000001_create_personal_access_tokens_table', 1);
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES (5, '2022_11_07_162408_create_products_table', 1);
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES (6, '2022_11_07_162659_create_reviews_table', 1);
