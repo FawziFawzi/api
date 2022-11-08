a3e5122cfe9d18103372b86c5cb352e49b9463fe
FawziFawzi <fawzi.fawzi2020@gmail.com>
2022-11-08 19:56:51 +0200

    finished the relations between product and review

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+    public function reviews(Type $var = null)
+    {
+        return $this->hasMany(Review::class,'products_id');
+    }
 }
--- app/Models/Review.php
+++ app/Models/Review.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Review extends Model
 {
     use HasFactory;
+    public function product(Type $var = null)
+    {
+        return $this->belongsTo(Product::class ,'products_id');
+    }
 }
