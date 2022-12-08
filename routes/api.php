94a3418ae837fa64ef9e309d858f3e1fd07be645
FawziFawzi <fawzi.fawzi2020@gmail.com>
2022-12-08 13:59:44 +0200

    passport installed

--- routes/api.php
+++ routes/api.php
@@ -16,8 +16,8 @@ use App\Http\Controllers\ReviewController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return response()->json($request->user()); ;
 });
 Route::apiResource('/products',ProductController::class);
 Route::group(['prefix'=>'products'], function(){
